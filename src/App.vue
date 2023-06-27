309472d08bfa1467fd556a796f89b22b823b139a
Abenathi201 <abenathi.sindaphi201@gmail.com>
2023-06-27 16:44:46 +0200

    Form done, NavBar almost done

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <NavBar />
+  <router-view/>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default{
+  components: { NavBar }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  background: #333;
+}
+
+nav {
+  padding: 30px;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,79 @@
+<template>
+    <!-- <nav class="topNav">
+        <routerlink class="active" to="/">Home</routerlink>
+        <routerlink to="/tutorials">Tutorial</routerlink>
+        <routerlink to="/about">About</routerlink>
+        <routerlink to="/Form">Form</routerlink>
+    </nav> -->
+
+    <nav>
+      <routerlink to="/" id="logo">My Logo</routerlink>
+      <input type="checkbox" id="hamburger" />
+      <label for="hamburger">
+        <i class="fa-solid fa-bars"></i>
+      </label>
+      <ul>
+        <li>
+          <routerlink to="/">Home</routerlink>
+        </li>
+        <li>
+            <routerlink to="/">About</routerlink>
+        </li>
+        <li>
+            <routerlink to="/">Products</routerlink>
+        </li>
+        <li>
+            <router-link to="/form">Contact Us</router-link>
+        </li>
+      </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+nav {
+  font-family: "Poppins", sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #d9d9d9;
+  padding: 1em;
+}
+nav a {
+  text-decoration: none;
+  text-transform: capitalize;
+  color: #ffffff;
+}
+#logo {
+  font-size: 1.2em;
+  font-weight: 600;
+}
+input[type="checkbox"] {
+  appearance: none;
+}
+label {
+  display: none;
+  cursor: pointer;
+}
+ul {
+  list-style: none;
+  display: flex;
+  gap: 1em;
+}
+ul a {
+  padding: 0.5em;
+}
+ul a:hover {
+  background-color: #333333;
+  border-radius: 0.5em;
+}
+.active {
+  border-bottom: 2px solid #ffffff;
+}
+
+</style>
